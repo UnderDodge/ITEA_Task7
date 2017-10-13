8ef8831aaafc13642a3a5070f74a2ff4318f0415
UnderDodge <assassinthug009@gmail.com>
2017-10-13 15:28:38 +0300

    2 commit Master

---
+++ LastOfUs/src/main/java/com/playstation/store/Fight.java
@@ -0,0 +1,11 @@
+package com.playstation.store;
+
+/**
+ * Created by Администратор on 13.10.2017.
+ */
+public class Fight {
+    public static void main (String [] args){
+        System.out.println("Start");
+        System.out.println("End");
+    }
+}
